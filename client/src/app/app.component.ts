e517eb77aa9143c0e7f09bc4a79dc09eb43ae77a
yongantouch <lyanchong44@gmail.com>
2021-03-17 23:21:22 +0700

    end of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +8,11 @@ import { IProduct } from './models/product';
 export class AppComponent implements OnInit{
   
   title = 'Skinet';
-  products: IProduct[];
 
-  constructor(private http: HttpClient){
+  constructor(){
     
   }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products').subscribe((response: any) =>{
-      this.products = response.data;
-      console.log(response);
-    }, error =>{
-      console.log(error);
-    });
   }
 }
---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,97 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
+import { IProduct } from '../shared/models/product';
+import { IType } from '../shared/models/productType';
+import { ShopParams } from '../shared/models/shopParams';
+import { ShopService } from './shop.service';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent implements OnInit {
+  @ViewChild('search',{ static: true }) searchTerm: ElementRef;
+  products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  shopParams = new ShopParams;
+  totalCount: number;
+
+  sortOption = [
+    {name: 'Alphabetical', value: 'name'},
+    {name: 'Price: Low to High', value: 'priceAsc'},
+    {name: 'Price: High to Low', value: 'priceDesc'}
+  ]
+
+  constructor(private shopService: ShopService) { }
+
+  ngOnInit(): void {
+    this.getProduct();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProduct(){
+    this.shopService.getProducts(this.shopParams)
+      .subscribe(response =>{
+      this.products = response.data;
+      this.shopParams.pageNumber = response.index;
+      this.shopParams.pageSize = response.pageSize;
+      this.totalCount = response.count;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getBrands(){
+    this.shopService.getBrands().subscribe(repsonse =>{
+      this.brands = [{id:0, name: 'All'}, ...repsonse];
+    }, error => {
+      console.log(error);
+    });
+  }
+  getTypes(){
+    this.shopService.getTypes().subscribe(repsonse =>{
+      this.types = [{id:0, name: 'All'}, ...repsonse];
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  onBrandIdSelected(brandId: number){
+    this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
+    this.getProduct();
+  }
+
+  onTypeIdSelected(typeId: number){
+    this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
+    this.getProduct();
+  }
+
+  onSortSelected(sort:string){
+    this.shopParams.sort = sort;
+    this.getProduct();
+  }
+
+  onPageChanged(event: any){
+    if(this.shopParams.pageNumber !== event){
+      this.shopParams.pageNumber = event;
+      this.getProduct();
+    }
+  }
+
+  onSearch(){
+    this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
+    this.getProduct();
+  }
+
+  onRest(){
+    this.searchTerm.nativeElement.value = '';
+    this.shopParams = new ShopParams();
+    this.getProduct()
+  }
+}
